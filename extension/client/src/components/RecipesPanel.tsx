31ea5bff02599f9f4a8031c9baeb9d09706eb5ec
Raymond Selzer <raymond@raymondselzer.net>
2025-11-17 11:12:49 -0500

    cleanup

--- extension/client/src/components/RecipesPanel.tsx
+++ extension/client/src/components/RecipesPanel.tsx
@@ -1,21 +1,43 @@
-import { useCallback } from "react"
+import { useCallback, useState } from "react"
 
-import { Button, List } from "antd"
+import { Button, Collapse, List, Splitter } from "antd"
 
 import { css } from "@emotion/react"
-import { fetchCartProducts } from "../api/voila"
+
 import { useStore } from "../store"
 import { CloseOutlined } from "@ant-design/icons"
 import { UnstyledButton } from "./common/elements.styles"
-
+import Markdown from "react-markdown"
 export function RecipesPanel() {
-	const handleGetRecipesClick = useCallback(() => {
-		fetchCartProducts().then((products) => {
-			console.log("Cart products:", products)
-		})
-	}, [])
-
 	const { ingredients, removeIngredient } = useStore()
+	const [recipes, setRecipes] = useState<Record<string, unknown>[]>([])
+	const handleGetRecipesClick = useCallback(() => {
+		fetch(
+			"https://raymond8505.app.n8n.cloud/webhook-test/voila-shopper-recipes",
+			{
+				body: JSON.stringify({
+					ingredients: ingredients.map((ingredient) => {
+						return [
+							"productId",
+							"name",
+							"packageSizeDescription",
+							"price",
+							"promoPrice",
+							"unitPrice",
+							"promoUnitPrice",
+							"categoryPath",
+						].reduce((acc, key) => {
+							acc[key] = ingredient[key]
+							return acc
+						}, {})
+					}),
+				}),
+				method: "POST",
+			}
+		).then(async (resp) => {
+			setRecipes(JSON.parse((await resp.json()).content.parts[0].text))
+		})
+	}, [setRecipes])
 
 	return (
 		<div
@@ -36,47 +58,117 @@ export function RecipesPanel() {
 					Get Recipes
 				</Button>
 			</div>
+
 			<div
 				css={css`
 					overflow: auto;
 					padding: 8px;
 				`}
 			>
-				<strong>Ingredients</strong>
-				<List
-					itemLayout="horizontal"
-					dataSource={ingredients}
-					renderItem={(product) => (
-						<List.Item
-							actions={[
-								<UnstyledButton
-									onClick={() => removeIngredient(product.productId)}
-								>
-									<CloseOutlined />
-								</UnstyledButton>,
-							]}
+				<Splitter layout="vertical">
+					<Splitter.Panel min={188}>
+						<div
+							css={css`
+								padding: 8px 0;
+							`}
 						>
-							<List.Item.Meta
-								avatar={
-									product.images && product.images.length > 0 ? (
-										<img
-											alt={product.name}
-											src={product.images[0].src}
-											style={{ width: 50, height: 50, objectFit: "cover" }}
+							<strong>Ingredients</strong>
+							<List
+								itemLayout="horizontal"
+								dataSource={ingredients}
+								renderItem={(product) => (
+									<List.Item
+										actions={[
+											<UnstyledButton
+												onClick={() => removeIngredient(product.productId)}
+											>
+												<CloseOutlined />
+											</UnstyledButton>,
+										]}
+									>
+										<List.Item.Meta
+											avatar={
+												product.images && product.images.length > 0 ? (
+													<img
+														alt={product.name}
+														src={product.images[0].src}
+														style={{
+															width: 50,
+															height: 50,
+															objectFit: "cover",
+														}}
+													/>
+												) : null
+											}
+											title={product.name}
+											description={`Price: ${
+												product.promoPrice
+													? product.promoPrice.amount
+													: product.price.amount
+											}`}
 										/>
-									) : null
-								}
-								title={product.name}
-								description={`Price: ${
-									product.promoPrice
-										? product.promoPrice.amount
-										: product.price.amount
-								}`}
-							/>
-						</List.Item>
-					)}
-				></List>
-				<strong>Recipes</strong>
+									</List.Item>
+								)}
+							></List>
+						</div>
+					</Splitter.Panel>
+					<Splitter.Panel>
+						<div
+							css={css`
+								margin: 8px 0;
+							`}
+						>
+							<strong>Recipes</strong>
+							<Collapse>
+								{recipes.map((recipe) => {
+									return (
+										<Collapse.Panel
+											header={recipe.title as string}
+											key={recipe.title as string}
+										>
+											<div
+												css={css`
+													line-height: 1.2;
+
+													h2 {
+														font-size: 1.5em;
+														font-weight: 100;
+
+														&:not(:first-of-type) {
+															margin-top: 1.5em;
+															padding-top: 0.5em;
+															border-top: 1px solid rgba(0, 0, 0, 0.4);
+														}
+													}
+													h3 {
+														font-size: 1.2em;
+														font-weight: bold;
+													}
+
+													ul,
+													ol {
+														margin: 8px 0;
+														padding-left: 1.5em;
+													}
+
+													ul {
+														list-style: disc;
+													}
+
+													ol {
+														list-style: decimal;
+													}
+												`}
+											>
+												<Markdown>{recipe.content as string}</Markdown>
+											</div>
+										</Collapse.Panel>
+									)
+								})}
+							</Collapse>
+						</div>
+					</Splitter.Panel>
+				</Splitter>
 			</div>
 		</div>
 	)
