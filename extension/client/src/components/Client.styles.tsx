58af9844eb2d5d01055f6b69894ca2d6eefe795c
Raymond Selzer <raymond@raymondselzer.net>
2025-12-08 23:21:51 -0500

    Client styles

---
+++ extension/client/src/components/Client.styles.tsx
@@ -0,0 +1,31 @@
+import styled from "@emotion/styled"
+
+export const Wrapper = styled.div<{ drawerOpen: boolean }>`
+	width: 25vw;
+	height: 100vh;
+	min-width: 360px;
+	position: fixed;
+	top: 0;
+	left: 0;
+	background-color: #f9f3eb;
+	box-shadow: 2px 0 5px rgba(0, 0, 0, 0.1);
+	padding: 8px;
+	z-index: 999999999;
+	transition: transform 200ms ease-out;
+
+	> div,
+		.ant-tabs-tabpane,
+		.ant-tabs-content-holder,
+		.ant-tabs,
+		.ant-tabs-content {
+			height: 100%;
+		}
+	}
+
+	${({ drawerOpen }) =>
+		drawerOpen
+			? ""
+			: `
+					transform: translateX(-100%);	
+				`}
+`
--- extension/client/src/components/Client.tsx
+++ extension/client/src/components/Client.tsx
@@ -1,4 +1,3 @@
-import { css } from "@emotion/react"
 import Tabs from "antd/es/tabs"
 import type { TabsProps } from "antd/es/tabs"
 import { ProductsPanel } from "./ProductsPanel"
@@ -7,6 +6,7 @@ import { SettingsPanel } from "./SettingsPanel"
 
 import { useStore } from "../store"
 import { DrawerButton } from "./DrawerButton"
+import { Wrapper } from "./Client.styles"
 
 export function Client() {
 	const { drawerOpen } = useStore()
@@ -29,41 +29,11 @@ export function Client() {
 		},
 	]
 	return (
-		<div
-			css={css`
-				width: 25vw;
-				height: 100vh;
-				min-width: 360px;
-				position: fixed;
-				top: 0;
-				left: 0;
-				background-color: #f9f3eb;
-				box-shadow: 2px 0 5px rgba(0, 0, 0, 0.1);
-				padding: 8px;
-				z-index: 999999999;
-				transition: transform 200ms ease-out;
-
-				${drawerOpen
-					? ""
-					: `
-					transform: translateX(-100%);	
-				`}
-			`}
-		>
-			<div
-				css={css`
-					&,
-					.ant-tabs-tabpane,
-					.ant-tabs-content-holder,
-					.ant-tabs,
-					.ant-tabs-content {
-						height: 100%;
-					}
-				`}
-			>
+		<Wrapper drawerOpen={drawerOpen}>
+			<div>
 				<Tabs defaultActiveKey="recipes" items={items} />
 			</div>
 			<DrawerButton />
-		</div>
+		</Wrapper>
 	)
 }
